0b6126d52cbde05b0e42cde46949391218b23425
Jedza83 <gmejlana@gmail.com>
2023-12-06 15:35:06 +0100

    css

--- src/components/WeatherBody.js
+++ src/components/WeatherBody.js
@@ -4,7 +4,6 @@ function WeatherBody() {
   return (
     <div className="centerDiv">
       <img className="cloudImg" src={cloud} alt="cloud" />
-
       <h1 className="degNumber">19&deg;C</h1>
       <p>Precipitations: Max 19&deg;C - Min 10&deg;C</p>
     </div>
